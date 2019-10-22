d32f627f02c9b2327c89089dfa2a9275ffdf0c7b
walter6765jon <56731531+walter6765jon@users.noreply.github.com>
2019-10-23 03:10:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/browse/jewelry/promise-rings/3891_540912_1228412");
+header("Location: https://support.office.com/en-us/article/download-and-install-or-reinstall-office-365-or-office-2019-on-a-pc-or-mac-4414eaaf-0478-48be-9c42-23adc4716658?wt.mc_id=O365_Cons_HUP_Footer_DownloadandInstall&ui=en-US&rs=en-US&ad=US");
 die();
 ?>
