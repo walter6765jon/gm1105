bfab4287458a285f1d59e2b698d39d7c64dafe6e
walter6765jon <56731531+walter6765jon@users.noreply.github.com>
2020-10-03 18:16:37 -0700

    Update and rename site-index.php to index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: https://www.turnerpest.com/commercial/services/");
+die();
+?>
--- site-index.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Location: https://www.onlineshoes.com/US/en/hush-puppies-outlet-1/");
-die();
-?>
